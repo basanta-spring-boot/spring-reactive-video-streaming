c5d025bb3db6c2383b8eeede91f6ea34411ef0ba
Basant kumar Hota <basahota@users.noreply.github.com>
2021-08-23 23:53:30 +0530

    Add files via upload

---
+++ src/main/java/com/javatechie/streaming/SpringbootVideoStreamingApplication.java
@@ -0,0 +1,16 @@
+package com.javatechie.streaming;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+
+@SpringBootApplication
+public class SpringbootVideoStreamingApplication  {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVideoStreamingApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/javatechie/streaming/SpringbootVideoStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.streaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootVideoStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
